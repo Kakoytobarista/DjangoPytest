0f418e724a82aa5f3f6998c39d5ecccb39ea8c1b
Aslan <baristan1@rambler.ru>
2020-10-20 22:41:16 +0300

    add project

---
+++ webapp/webapp/urls.py
@@ -0,0 +1,12 @@
+from django.conf import settings
+from django.conf.urls.static import static
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('main.urls')),
+    path('news/', include('news.urls'))
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
